6549c3a1e78a565ee938443984440bdc9c5b4246
krakozaybp <krakozaybp@gmail.com>
2022-05-06 20:35:36 +0700

    some corrections in Post model

--- web/posts/models.py
+++ web/posts/models.py
@@ -8,11 +8,11 @@ User = get_user_model()
 
 
 class Post(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
-    title = models.TextField(max_length=64, default='title', verbose_name='Заголовок')
+    title = models.CharField(max_length=64, default='title', verbose_name='Заголовок')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True, verbose_name='Автор')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория', null=True)
     text = models.TextField(max_length=200, default='text', verbose_name='Текст')
     tags = models.ManyToManyField(Tag, verbose_name='Тэги', blank=True)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категории', null=True)
 
     def __str__(self):
         return self.title
