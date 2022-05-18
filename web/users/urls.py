3a39edf16f91c70058ffd57d1a51933a05c68261
krakozaybp <krakozaybp@gmail.com>
2022-05-18 22:28:11 +0700

    users/profile is working now

--- web/users/urls.py
+++ web/users/urls.py
@@ -13,8 +13,8 @@ app_name = "users"
 
 urlpatterns = [
     path("", UserListView.as_view(), name="user_list"),
-    path("<username>/", UserDetailView.as_view(), name="user_detail"),
     path("profile/", ProfileView.as_view(), name="profile"),
+    path("<username>/", UserDetailView.as_view(), name="user_detail"),
     path("validate_username", validate_username, name="validate_username"),
     path("validate_email", validate_email, name="validate_email"),
 ]
