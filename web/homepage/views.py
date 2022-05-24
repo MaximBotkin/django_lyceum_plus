eebd3cfd691f2ea7e56652f8ea82391461373167
MaximBotkin <botkin150@gmail.com>
2022-05-24 15:18:43 +0300

    add search bar

--- web/homepage/views.py
+++ web/homepage/views.py
@@ -2,6 +2,7 @@ from django.views.generic import TemplateView
 from django.views.generic.base import ContextMixin
 from posts.models import Post
 from description.models import Category
+from django.shortcuts import render
 
 
 class HomepageView(TemplateView, ContextMixin):
@@ -12,3 +13,12 @@ class HomepageView(TemplateView, ContextMixin):
         context['posts'] = Post.objects.get_collection(self.request.user)
         context['categories'] = Category.objects.all()
         return context
+
+    def post(self, request, *args, **kwargs):
+        context = {}
+        context['categories'] = Category.objects.all()
+        result = []
+        if request.POST['input']:
+            result = Post.objects.filter(title__contains=request.POST['input']).order_by('-creation_date')
+        context['posts'] = result
+        return render(request, self.template_name, context)
